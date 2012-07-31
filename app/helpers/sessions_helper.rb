0fee13c7c3a55681f49870091c38a91de8433833
ts3m <matt.tse.cs@gmail.com>
2012-07-30 19:17:45 -0700

    set cookies to expire in 30 minutes

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
 
   def sign_in(user)
-    cookies.permanent[:remember_token] = user.remember_token
+    cookies[:remember_token] = { value: user.remember_token, expires: 30.minutes.from_now}
     self.current_user = user
   end
 
